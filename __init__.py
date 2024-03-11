bdcd4295ba31a0769b54daf0ecc9fd3394a697be
Badio95 <75184833+Badio95@users.noreply.github.com>
2024-03-11 10:19:05 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -35,6 +35,9 @@ def mongraphique():
 def mongraphique2():
     return render_template("histogramme.html")
 
+@app.route("/commits/")
+def moncommits():
+    return render_template("commits.html")
   
 if __name__ == "__main__":
   app.run(debug=True)
